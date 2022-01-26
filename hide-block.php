6b3be520bcf56394a7a52683e81262a1e12ddffb
Benjamin Zekavica <info@benjamin-zekavica.de>
2022-01-26 21:35:14 +0100

    Update hide-block.php

--- hide-block.php
+++ hide-block.php
@@ -3,7 +3,7 @@
 Plugin Name:       Hide Block
 Description:       Gutenberg Toolkit - Hide Backend Blocks
 Plugin URI:        https://wordpress.org/plugins/hide-block
-Version:           1.1.0
+Version:           1.2.0
 Author:            Benjamin Zekavica
 Author URI:        https://www.benjamin-zekavica.de
 License:           GPL v2 or later
@@ -24,12 +24,9 @@ You should have received a copy of the GNU General Public License
 along with Easy SVG. If not, see license.txt .
 
 Copyright by:
-© 2021 by Benjamin Zekavica. All rights reserved.
+© 2022 by Benjamin Zekavica. All rights reserved.
 Imprint:
 Benjamin Zekavica
-Oranienstrasse 12
-52066 Aachen
-
 E-Mail: info@benjamin-zekavica.de
 Web: www.benjamin-zekavica.de
 
